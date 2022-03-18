0529a7e91814df9dc6bd9a4a46cefc5396f33b03
Burak Aykanat <burak.aykanat12@gmail.com>
2022-03-18 11:57:54 +0300

    Delete src directory

--- src/main/java/com/n11/conference/repository/ConferenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.n11.conference.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.n11.conference.entity.ConferencePlan;
-
-public interface ConferenceRepository extends CrudRepository<ConferencePlan, Long> {
-
-}
